167de190201bb4e960448bdf5813f487366b9f46
Yaroslav Sytnyk <gvamburg@gmail.com>
2023-08-16 17:41:14 +0300

    update template for send mail

---
+++ resources/views/mail/feedback.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <span style="font-weight: bold;padding-right: 10px;"><b>П.І.Б:</b></span><span>{{ $fullName }}</span>
+    <span style="font-weight: bold;padding-right: 10px;"><b>Телефон:</b></span><span>{{ $phone }}</span>
+    <span style="font-weight: bold;padding-right: 10px;"><b>Місто:</b></span><span>{{ $city }}</span>
+    <span style="font-weight: bold;padding-right: 10px;"><b>Відгук:</b></span><span>{{ $feedback }}</span>
+</div>
