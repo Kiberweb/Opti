956ce0aac301f56671596bc4856cd0972c55cf5f
Yaroslav Sytnyk <gvamburg@gmail.com>
2023-08-16 12:52:45 +0300

    add item for migration

--- database/migrations/2023_08_16_075327_create_cities_table.php
+++ database/migrations/2023_08_16_075327_create_cities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->string('city')->nullable(false)->comment('City name');
+            $table->string('city', 27)->nullable(false)->comment('City name');
             $table->boolean('visible')->default(true)->comment('show hide city.');
             $table->timestamps();
         });
