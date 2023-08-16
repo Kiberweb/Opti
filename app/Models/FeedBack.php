159e3d92d35156bb6746070d3494c6c94b509a48
Yaroslav Sytnyk <gvamburg@gmail.com>
2023-08-16 12:48:05 +0300

    add relation in City and FeedBack

--- app/Models/FeedBack.php
+++ app/Models/FeedBack.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FeedBack extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'full_name',
+        'phone',
+        'city_id',
+        'feedback',
+    ];
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }
